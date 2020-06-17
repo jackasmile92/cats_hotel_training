bc82a335e6638096dcb10c0a1428d5761b17405d
jackasmile92 <jackasmile92@gmail.com>
2020-06-17 17:41:38 +0300

    generate room from input

--- js/input.js
+++ js/input.js
@@ -4,7 +4,8 @@ let input = [{
         image: "econom_cat.png",
         size: "Размеры (ШхГхВ) - 90х70х180 см",
         square: "Площадь - 0,63 м2",
-        price: "100₽"
+        price: "100₽",
+        equipment: ["empty.png"]
 
     },
     {
@@ -13,7 +14,8 @@ let input = [{
         image: "econom_plus_cat.png",
         size: "Размеры (ШхГхВ) - 90х100х180 см",
         square: "Площадь - 0,90 м2",
-        price: "200₽"
+        price: "200₽",
+        equipment: ["bed.png", "claw.png"]
 
     },
     {
@@ -22,7 +24,8 @@ let input = [{
         image: "comfort_cat.png",
         size: "Размеры (ШхГхВ) - 100х125х180 см",
         square: "Площадь - 1,13 м2",
-        price: "250₽"
+        price: "250₽",
+        equipment: ["bed.png", "claw.png", "games.png"]
 
 
     },
@@ -32,7 +35,8 @@ let input = [{
         image: "suite_cat.png",
         size: "Размеры (ШхГхВ) - 125х125х180 см",
         square: "Площадь - 1,56 м2",
-        price: "350₽"
+        price: "350₽",
+        equipment: ["bed.png", "claw.png", "games.png"]
 
 
     },
@@ -42,7 +46,9 @@ let input = [{
         image: "lux_cat.png",
         size: "Размеры (ШхГхВ) - 160х160х180 см",
         square: "Площадь - 2,56 м2",
-        price: "500₽"
+        price: "500₽",
+        equipment: ["bed.png", "claw.png", "games.png", "house.png"]
+
 
 
     },
@@ -52,12 +58,14 @@ let input = [{
         image: "super_lux_cat.png",
         size: "Размеры (ШхГхВ) - 180х160х180 см",
         square: "Площадь - 2,88 м2",
-        price: "600₽"
+        price: "600₽",
+        equipment: ["bed.png", "claw.png", "games.png", "house.png"]
 
     }
 ]
 
 let imageSrc = "./src/img/png/cats_room/";
+let imageSrc_1 = "./src/img/png/equipment/";
 
 function addAvatar(unit) {
     let avatar = document.createElement('div');
@@ -72,13 +80,112 @@ function addAvatar(unit) {
 
 }
 
+function addIcons(imageName) {
+    let avatar = document.createElement('img');
+    avatar.className = "room_equip";
+    avatar.src = imageSrc_1 + imageName;
+
+    return avatar;
+
+}
+
+
 
 function generateRoom(room) {
     let roomCard = document.createElement('div');
     roomCard.className = "result__room";
 
     roomCard.appendChild(addAvatar(room));
+
+    let roomResult = document.createElement('div');
+    roomResult.className = "result__room_body";
+
+    let roomtitle = document.createElement('div');
+    roomtitle.className = "result__room_title";
+    roomtitle.innerHTML = room.name;
+
+    roomResult.appendChild(roomtitle);
+
+    let roomtext_1 = document.createElement('div');
+    roomtext_1.className = "result__room_text";
+    roomtext_1.innerHTML = room.size;
+
+    roomResult.appendChild(roomtext_1);
+
+    let roomtext_2 = document.createElement('div');
+    roomtext_2.className = "result__room_text";
+    roomtext_2.innerHTML = room.square;
+
+    roomResult.appendChild(roomtext_2);
+
+    let roomtext_5 = document.createElement('div');
+    roomtext_5.className = "result__room_equip";
+
+    let roomtext_5_1 = document.createElement('div');
+    roomtext_5_1.className = "result__room_text";
+    roomtext_5_1.innerHTML = "Оснащение номера";
+
+    let roomtext_5_2 = document.createElement('div');
+    roomtext_5_2.className = "room_equip__content";
+
+    let rommEq = room.equipment;
+   
+    for (let i = 0; i < rommEq.length; i++) {
+
+        roomtext_5_2.appendChild(addIcons(rommEq[i]));
+    }
+
+
+    roomtext_5.appendChild(roomtext_5_1);
+    roomtext_5.appendChild(roomtext_5_2);
+
+
+    roomResult.appendChild(roomtext_5);
+
+    let roomtext_3 = document.createElement('div');
+    roomtext_3.className = "result__room_equip";
+
+    let roomtext_3_1 = document.createElement('div');
+    roomtext_3_1.className = "result__room_text";
+    roomtext_3_1.innerHTML = "Цена за сутки:";
+
+    let roomtext_3_2 = document.createElement('div');
+    roomtext_3_2.className = "result__room_price";
+    roomtext_3_2.innerHTML = room.price;
+
+    roomtext_3.appendChild(roomtext_3_1);
+    roomtext_3.appendChild(roomtext_3_2);
+
+
+    roomResult.appendChild(roomtext_3);
+
+    
+    
+
+
+    let roomtext_4 = document.createElement('div');
+    roomtext_4.className = "room__button";
+
+    let roomtext_4_1 = document.createElement('div');
+    roomtext_4_1.className = "room__button-text";
+    roomtext_4_1.innerHTML = "Забронировать";
+
+    let roomtext_4_2 = document.createElement('div');
+    roomtext_4_2.className = "room__button-paw";
+    roomtext_4_2.innerHTML = '<img src="./src/img/png/paw_reverse.png" alt="">';
+
+    roomtext_4.appendChild(roomtext_4_1);
+    roomtext_4.appendChild(roomtext_4_2);
+
+    roomResult.appendChild(roomtext_4);
+    roomCard.appendChild(roomResult);
+
     return roomCard;
 }
 
-document.body.appendChild(generateRoom(input[0]));
+let element = document.getElementById("searching__result_content");
+
+for (let i = 0; i < input.length; i++) {
+
+    element.appendChild(generateRoom(input[i]));
+}
